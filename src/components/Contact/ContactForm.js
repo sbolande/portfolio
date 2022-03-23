570f7c5aeb030d5adf60d347314306ad730197e8
Seth Bolander <bol15015@byui.edu>
2022-03-22 23:08:47 -0600

    finished implementing ContactForm submit

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -1,3 +1,4 @@
+import { useState, useRef } from "react";
 import {
   chakra,
   theme,
@@ -12,28 +13,88 @@ import {
   Textarea,
 } from "@chakra-ui/react";
 import { HiAtSymbol, HiOutlineUser } from "react-icons/hi";
-import { BiPaperPlane, BiBuildings } from "react-icons/bi";
+import { BiPaperPlane, BiBuildings, BiCheck } from "react-icons/bi";
+
+import emailjs from "@emailjs/browser";
+// Set Environment variables
+const SERVICE_ID = process.env.REACT_APP_SERVICE_ID;
+const TEMPLATE_ID = process.env.REACT_APP_TEMPLATE_ID;
+const USER_ID = process.env.REACT_APP_USER_ID;
 
 export default function ContactForm() {
   const toast = useToast();
+  const toastId = "already-sent";
 
-  const handleSubmit = (e) => {
+  const form = useRef();
+
+  const [messageSent, setMessageSent] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendEmail = (e) => {
     e.preventDefault();
-    toast({
-      title: "Message Sent!",
-      description: "I'll get back to you as soon as I can!",
-      status: "success",
-      position: "bottom",
-      containerStyle: {
-        marginBottom: "4rem",
-      },
-      duration: 9000,
-      isClosable: true,
-    });
+
+    // if successful message has already been sent require reload
+    if (messageSent) {
+      if (!toast.isActive(toastId)) {
+        toast({
+          id: toastId,
+          title: "Already Sent",
+          description:
+            "You've already sent a message. If you want to send another please reload the page.",
+          status: "info",
+          position: "bottom",
+          duration: 9000,
+          isClosable: true,
+        });
+      }
+      return;
+    }
+
+    // else send message to Email.js
+    setIsLoading(true);
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, USER_ID)
+      .then(
+        (res) => {
+          if (res.text === "OK") setMessageSent(true);
+          toast({
+            id: "success",
+            title: "Message Sent",
+            description: "I'll get back to you as soon as I can!",
+            status: "success",
+            position: "bottom",
+            duration: 9000,
+            isClosable: true,
+          });
+        },
+        (err) => {
+          console.log(err);
+          setMessageSent(false);
+          toast({
+            id: "error",
+            title: "Uh Oh!",
+            description: "There was a problem sending your message.",
+            status: "error",
+            position: "bottom",
+            duration: 9000,
+            isClosable: true,
+          });
+        }
+      )
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
-    <chakra.form margin="auto" w="85vw" maxW="600px" onSubmit={handleSubmit}>
+    <chakra.form
+      ref={form}
+      id="contactForm"
+      margin="auto"
+      w="85vw"
+      maxW="600px"
+      onSubmit={sendEmail}
+    >
       <VStack spacing="1rem">
         <FormControl isRequired>
           <FormLabel htmlFor="email" color="cyan.300">
@@ -45,7 +106,7 @@ export default function ContactForm() {
               children={<HiAtSymbol color={theme.colors.cyan["400"]} />}
             />
             <Input
-              id="email"
+              name="email"
               type="email"
               placeholder="johndoe@example.com"
               focusBorderColor="cyan.400"
@@ -64,7 +125,7 @@ export default function ContactForm() {
               children={<HiOutlineUser color={theme.colors.cyan["400"]} />}
             />
             <Input
-              id="name"
+              name="name"
               placeholder="John Doe"
               focusBorderColor="cyan.400"
               required
@@ -72,7 +133,7 @@ export default function ContactForm() {
           </InputGroup>
         </FormControl>
         <FormControl>
-          <FormLabel htmlFor="org" color="cyan.300">
+          <FormLabel htmlFor="company" color="cyan.300">
             Company
           </FormLabel>
           <InputGroup>
@@ -81,7 +142,7 @@ export default function ContactForm() {
               children={<BiBuildings color={theme.colors.cyan["400"]} />}
             />
             <Input
-              id="org"
+              name="company"
               placeholder="Company / Organization / Group"
               focusBorderColor="cyan.400"
             />
@@ -92,7 +153,7 @@ export default function ContactForm() {
             Message
           </FormLabel>
           <Textarea
-            id="message"
+            name="message"
             placeholder="..."
             focusBorderColor="cyan.400"
             required
@@ -102,10 +163,18 @@ export default function ContactForm() {
           variant="outline"
           colorScheme="cyan"
           type="submit"
-          _hover={{ bg: "cyan.400", color: "cyan.800" }}
-          leftIcon={<BiPaperPlane />}
+          _hover={{
+            bg: !messageSent && "cyan.400",
+            color: !messageSent && "cyan.800",
+            cursor: !messageSent ? "pointer" : "default",
+          }}
+          cursor={!messageSent ? "pointer" : "default"}
+          leftIcon={!messageSent ? <BiPaperPlane /> : <BiCheck />}
+          loadingText="Sending"
+          isLoading={isLoading}
+          isDisabled={messageSent}
         >
-          Send
+          {!messageSent ? "Send" : "Message Sent"}
         </Button>
       </VStack>
     </chakra.form>
