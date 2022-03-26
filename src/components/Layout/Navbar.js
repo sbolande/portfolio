ff2f76d7c845f02110f2e48c46e153934d7ba823
Seth Bolander <bol15015@byui.edu>
2022-03-26 13:16:45 -0600

    added navbar icons

--- src/components/Layout/Navbar.js
+++ src/components/Layout/Navbar.js
@@ -1,5 +1,8 @@
 import NavLink from "../UI/NavLink";
 import { HStack } from "@chakra-ui/react";
+import { RiHome2Line, RiFileTextLine } from "react-icons/ri";
+import { BiPaperPlane } from "react-icons/bi";
+
 import { useLocation } from "react-router-dom";
 
 export default function Navbar() {
@@ -8,21 +11,28 @@ export default function Navbar() {
   return (
     <HStack
       justify="center"
-      spacing="3rem"
+      spacing="2.5rem"
       bg="gray.700"
       fontSize="1.15rem"
       marginBottom="1rem"
       boxShadow="0 0.2rem 1rem #171923"
     >
-      <NavLink name="Home" to="/" isActive={location === "/"} />
+      <NavLink
+        name="Home"
+        to="/"
+        icon={<RiHome2Line />}
+        isActive={location === "/"}
+      />
       <NavLink
         name="Projects"
         to="/projects"
+        icon={<RiFileTextLine />}
         isActive={location === "/projects"}
       />
       <NavLink
         name="Contact Me"
         to="/contact"
+        icon={<BiPaperPlane />}
         isActive={location === "/contact"}
       />
     </HStack>
